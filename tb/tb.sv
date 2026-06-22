78a8ed207564be2c14d119bbde45904ca8599f57
Nia Tharakan <niatharakan4@gmail.com>
2026-06-22 14:38:11 +0530

    Update and rename tb.sv to tb/tb.sv

--- tb/tb.sv
+++ tb/tb.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 11:22:28
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
