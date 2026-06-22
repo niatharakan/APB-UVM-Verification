d6355adf6f54eafc6121bbce0b6f7e05b05bb4e1
Nia Tharakan <niatharakan4@gmail.com>
2026-06-22 14:40:25 +0530

    Update and rename my_test.sv to tb/my_test.sv

--- tb/my_test.sv
+++ tb/my_test.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 10:11:48
-// Design Name: 
-// Module Name: my_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 class my_test extends uvm_test;
   `uvm_component_utils(my_test)
