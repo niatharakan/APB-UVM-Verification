a948f17c52a0cfdf27dc81a062b1decd5e5aa347
Nia Tharakan <niatharakan4@gmail.com>
2026-06-22 14:39:56 +0530

    Update and rename apb_monitor.sv to tb/apb_monitor.sv

--- tb/apb_monitor.sv
+++ tb/apb_monitor.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 15:02:14
-// Design Name: 
-// Module Name: apb_monitor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 import uvm_pkg::*;
 `include "uvm_macros.svh"
