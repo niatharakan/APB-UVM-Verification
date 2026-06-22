45641de9887484303e282290318068f45e18c101
Nia Tharakan <niatharakan4@gmail.com>
2026-06-22 14:35:58 +0530

    Update and rename apb_slave.sv to rtl/apb_slave.sv

--- rtl/apb_slave.sv
+++ rtl/apb_slave.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 16:22:36
-// Design Name: 
-// Module Name: apb_slave
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module apb_slave (
   input  logic        PCLK,
