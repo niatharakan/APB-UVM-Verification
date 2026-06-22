6ca0559e7fe1373db2437e0ec723f5a190adf6a0
Nia Tharakan <niatharakan4@gmail.com>
2026-06-22 14:39:21 +0530

    Update and rename apb_env.sv to tb/apb_env.sv

--- tb/apb_env.sv
+++ tb/apb_env.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.06.2026 16:13:48
-// Design Name: 
-// Module Name: apb_env
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 import uvm_pkg::*;
 `include "uvm_macros.svh"
